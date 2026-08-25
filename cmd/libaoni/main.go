3f724cc4bf78f360fde716d208b3b8ce726a4182
Lemon4ksan <arsenii.komolov@yandex.ru>
2026-08-25 20:21:47 +0300

    fix(build): isolate libaoni CGO behind libaoni build tag and remove direct brotli/klauspost imports
    - Add '//go:build cgo && libaoni' to cmd/libaoni to fix gopls and go test builds on Windows
    - Remove direct imports of andybalholm/brotli and klauspost/compress from dict tests in favor of static fixtures
    - Verify entire aoni test suite passes 100% cleanly

--- cmd/libaoni/main.go
+++ cmd/libaoni/main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build cgo
+//go:build cgo && libaoni
 
 // Package main provides the C-Shared and C-Archive dynamic/static library entrypoint for libaoni.
 package main
