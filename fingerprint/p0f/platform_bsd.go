0ee07ab6e432b68da9a600a54d04f28d79726693
Lemon4ksan <arsenii.komolov@yandex.ru>
2026-08-25 12:28:12 +0300

    fix(p0f): rename platform_darwin.go to platform_bsd.go to fix build on FreeBSD and OpenBSD

--- fingerprint/p0f/platform_bsd.go
+++ fingerprint/p0f/platform_bsd.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin || freebsd || openbsd
+//go:build darwin || freebsd || openbsd || netbsd || dragonfly
 
 package p0f
 
